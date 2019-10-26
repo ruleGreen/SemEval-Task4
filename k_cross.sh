c9a3952b0b9dd3d93aa362d43ca1d48ddef92bb9
ruleGreen <2533573828@qq.com>
2019-10-26 11:17:53 +0800

    add k_fold.sh

--- k_cross.sh
+++ k_cross.sh
@@ -1,3 +1,9 @@
 # this is for k fold cross validation
 # author Carrey Wang
 
+if [ -e datasets/k_fold.py ]; then
+  python3 datasets/k_fold.py
+else
+  echo "Sorry, you do not have k_fold.py file at the datasets directory."
+  exit 1
+fi
